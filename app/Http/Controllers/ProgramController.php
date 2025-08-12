272c9341ac4b338f7e060ea3f7306cdca0a699ec
gaikiir <gaichris380@gmail.com>
2025-08-12 21:52:21 +0300

    public program controller handle public program display

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Program;
 use Illuminate\Http\Request;
 
 class ProgramController extends Controller
 {
-    //
+    /**
+     * Display a listing of programs for the frontend.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showFrontendPrograms()
+    {
+        $programs = Program::paginate(6);
+        return view('programs', compact('programs'));
+    }
 }
