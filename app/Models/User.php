bad41f30a8a857d2b77d28942fa2027c48308e7b
gaikiir <gaichris380@gmail.com>
2025-08-12 22:04:59 +0300

    user model update with notification relationship updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+ 
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
@@ -34,20 +35,35 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeRecent($query)
+    {
+        return $query->latest()->take(2);
+    }
 
     /**
      * Get assignments assigned to this user.
      */
     public function assignments()
     {
-        return $this->belongsToMany(Assignment::class, 'assignment_student', 'user_id', 'assignment_id');
+        return $this->belongsToMany(Assignment::class, 'student_assignments', 'student_id', 'assignment_id');
+    }
+
+    public function courseApplications()
+    {
+        return $this->hasMany(CourseApplication::class, 'student_id');
+    }
+
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+    public function isStudent(){
+        return $this->role === 'student';
     }
 }
